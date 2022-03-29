37014310a00f1517b0cb1c552500072a829d06dc
MarvEdCV <71089107+MarvEdCV@users.noreply.github.com>
2022-03-29 10:33:23 -0600

    Create macros.asm
    Archivo de macros creado. Se generan diferentes macros para imprimir cadenas, recibir cadenas, limpiar pantalla y presionar enter.

---
+++ macros.asm
@@ -0,0 +1,19 @@
+PrintText macro Text ;Macro para imprimir textos
+    mov ah,09h
+    lea dx,offset Text
+    int 21h
+endm
+RecibirEntrada macro  ;Macro para recibir datos de entrada 
+    mov ah,01
+    int 21h
+endm
+Cls macro   ;Macro para limpiar la pantalla
+  mov  ah, 0
+  mov  al, 3
+  int  10H
+endm
+
+PressEnter macro
+  mov ah, 00
+  int 16h
+endm
